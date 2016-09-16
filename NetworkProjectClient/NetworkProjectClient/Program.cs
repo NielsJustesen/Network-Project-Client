df007648c1d1f152b319327df3684022723dc25f
NielsJustesen <niels449@hotmail.com>
2016-09-16 13:19:47 +0200

    Nuget RestSharp

--- NetworkProjectClient/NetworkProjectClient/Program.cs
+++ NetworkProjectClient/NetworkProjectClient/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace NetworkProjectClient
 {
@@ -10,6 +12,69 @@ namespace NetworkProjectClient
     {
         static void Main(string[] args)
         {
+            Connect("192.168.1.101");
+        }
+        static void Connect(String server)
+        {
+            bool con = true;
+            try
+            {
+                // Create a TcpClient. 
+                // Note, for this client to work you need to have a TcpServer  
+                // connected to the same address as specified by the server, port 
+                // combination.
+                while (con)
+                {
+
+                    Int32 port = 13000;
+                    TcpClient client = new TcpClient(server, port);
+
+                    String message = Console.ReadLine();
+                    // Translate the passed message into ASCII and store it as a Byte array.
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                    // Get a client stream for reading and writing. 
+                    //  Stream stream = client.GetStream();
+
+                    NetworkStream stream = client.GetStream();
+
+                    // Send the message to the connected TcpServer. 
+                    stream.Write(data, 0, data.Length);
+
+                    Console.WriteLine("Sent: {0}", message);
+
+                    // Receive the TcpServer.response. 
+
+                    // Buffer to store the response bytes.
+                    data = new Byte[256];
+
+                    // String to store the response ASCII representation.
+                    String responseData = String.Empty;
+
+                    // Read the first batch of the TcpServer response bytes.
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received: {0}", responseData);
+                    if (responseData == "EXIT")
+                    {
+                        con = false;
+                    }
+                    // Close everything.
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
         }
     }
 }
